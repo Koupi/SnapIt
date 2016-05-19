322b7586bb7db5f7c5b25fbc5b0fdd7cdcba8c92
Koupi <sasha3849@yandex.ru>
2016-05-19 14:28:03 +0300

    Project added.

---
+++ SnapIt/SnapIt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SnapIt
+//
+//  Created by Admin on 19.05.16.
+//  Copyright © 2016 Alex Zhidkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
